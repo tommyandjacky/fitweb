151220efeb406ea3c104f92be747dc11dc1e7cb2
tommy <tommyandjacky@gmail.com>
2014-07-08 09:51:37 +0800

    fist commit

---
+++ README.md
@@ -0,0 +1,4 @@
+fitweb
+====
+
+基于FIS的纯前端模块化解决方案fitweb
---
+++ fitweb.js
@@ -0,0 +1,104 @@
+﻿var fis = module.exports = require('fis');
+
+fis.cli.name = 'fitweb';
+fis.cli.info = fis.util.readJSON(__dirname+'/package.json');
+
+fis.require.prefixes.unshift('fitweb');
+
+fis.config.merge({
+    modules: {
+        parser: {
+            less: 'less',
+            tmpl : 'nunjucks'
+        },
+        postprocessor: {
+            js: "jswrapper, require-async",
+            html: "require-async"
+        },
+        postpackager : ['autoload', 'simple'],
+        lint : {
+            js : 'jshint'
+        }
+    },
+    roadmap: {
+        ext: {
+            less: 'css',
+            tmpl : 'js'
+        },
+        path : [
+            {
+                //less的mixin文件无需发布
+                reg : /^\/app\/(.*)mixin\.less$/i,
+                release : false
+            },
+            {
+                //其他css文件
+                reg : /^\/app\/(.*)\.(css|less)$/i,
+                //css文件会做csssprite处理
+                useSprite : true,
+                release : '/$1.$2'
+            },
+            {
+                //前端模板
+                reg: /^\/app\/(.*)\.tmpl$/i,
+                //当做类js文件处理，可以识别__inline, __uri等资源定位标识
+                isJsLike: true,
+                release: "$1.tmpl.js"
+            },
+            {
+                reg : /\/app\/(.*)\.(html|jsp|tpl|vm|htm|asp|aspx)/,
+                useCache : false,
+                release : '/$1.$2'
+            },
+            {
+                reg : /^\/app\/(.*)$/i,
+                useCache : false,
+                release : '$1'
+            },
+            {
+                reg : /^\/app_lib\/(.*)$/i,
+                useCache : false,
+                release : '/lib/$1'
+            },
+            {
+                reg : /^\/app_server\/(.*)$/i,
+                useCompile : false,
+                release : '/server/$1'
+            },
+            {
+                reg : /^\/app_modules\/([^\/]+)\/dist\/(.*)$/i,
+                useCache : false,
+                release : '/modules/$1/$2'
+            },
+            {
+                reg : /^\/pkg\/(.*)$/i,
+                release : '/pkg/$1'
+            },
+            {
+                reg : "**",
+                release : false
+            }
+        ]
+    },
+    settings: {
+        postprocessor: {
+            jswrapper: {
+                type: 'amd'
+            }
+        },
+        jshint : {
+            //using Chinese reporter
+            i18n : 'zh-CN',
+            camelcase : true,
+            curly : true,
+            eqeqeq : true,
+            forin : true,
+            immed : true,
+            latedef : true,
+            newcap : true,
+            noarg : true,
+            noempty : true,
+            node : true
+        }
+    }
+});
